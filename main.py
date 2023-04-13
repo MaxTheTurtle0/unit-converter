856bbba4a23ccb475cea59cb28d2e7458efae265
Andrei <m.andrei.antonescu@gmail.com>
2023-04-13 13:10:40 +0200

    changed the GUI to look better, created an output_box and restricted input_box to numbers only

--- main.py
+++ main.py
@@ -2,43 +2,46 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Unit Converter")
-window.geometry("700x300")
+window.geometry("500x300")
 
 #this is so that the user knows what unit he is converting
-T = tk.Label(window, text="Temperature", font=("Arial", 24))
-T.pack(pady=20)
-
-#this is where the user is supposed to write his unit he wants converted
-input_box = tk.Entry(window, width=12, font=("Arial", 14))
-input_box.pack(side="left", padx="20", pady="20")
+T = tk.Label(window, text="Temperature", font=("Arial", 24), bg="lightgrey")
+T.pack(fill="both")
 
 #this is where the user can see the result of the conversion
 output_box = tk.Text(window, width="12", height="1", font=("Arial", 14))
 output_box.configure(state="disabled")
-output_box.pack(side="left", padx="20", pady="20")
+output_box.pack(side="right", padx="20", pady="20")
+
+#this is where the user is supposed to write his unit he wants converted
+input_box = tk.Entry(window, width=12, font=("Arial", 14))
+# set the input type to a number
+input_box.config(validate="key")
+input_box.config(validatecommand=(input_box.register(lambda val: val.isdigit() or val == "." or val == "-"), '%S'))
+input_box.pack(side="right", padx="20", pady="20")
 
 #this button frame is where all the buttons are which are used for manoeuvring through the app
-buttonframe = tk.Frame(window)
-buttonframe.rowconfigure(0, weight=1)
-buttonframe.rowconfigure(1, weight=1)
-buttonframe.rowconfigure(2, weight=1)
-buttonframe.rowconfigure(3, weight=1)
-buttonframe.columnconfigure(0, weight=1)
+sidebar = tk.Frame(window, background="lightgrey")
+sidebar.rowconfigure(0, weight=1)
+sidebar.rowconfigure(1, weight=1)
+sidebar.rowconfigure(2, weight=1)
+sidebar.rowconfigure(3, weight=1)
+sidebar.columnconfigure(0, weight=1)
 
 #these buttons changing to the unit which you want to convert(Temperature, Length, Area, Volume)
-T_button = tk.Button(buttonframe, text="Temperature", font=("Arial", 16))
-T_button.grid(row=0, column=0, sticky="nsew")
+T_button = tk.Button(sidebar, text="Temperature", font=("Arial", 16))
+T_button.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
 
-L_button = tk.Button(buttonframe, text="Length", font=("Arial", 16))
-L_button.grid(row=1, column=0)
+L_button = tk.Button(sidebar, text="Length", font=("Arial", 16))
+L_button.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
 
-A_button = tk.Button(buttonframe, text="Area", font=("Arial", 16))
-A_button.grid(row=2, column=0)
+A_button = tk.Button(sidebar, text="Area", font=("Arial", 16))
+A_button.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
 
-V_button = tk.Button(buttonframe, text="Volume", font=("Arial", 16))
-V_button.grid(row=3, column=0)
+V_button = tk.Button(sidebar, text="Volume", font=("Arial", 16))
+V_button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 
-buttonframe.pack(fill="both", expand=True, side="left", padx=20, pady=20, anchor="nw")
+sidebar.pack(side="left", fill="y")
 
 window.mainloop()   
