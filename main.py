12bf7e637ca5a84037a8992946ceacefd661b94b
Kali <m.andrei.antonescu@gmail.com>
2023-04-15 14:35:59 +0000

    made windows not resizable, added an OptionMenu for the converted unit and changed the size of the TLabel

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ import customtkinter
 window = ctk.CTk()
 window.title("Unit Converter")
 window.geometry("500x300")
+window.resizable(False, False)
+
 ctk.set_appearance_mode("dark")
 #this is so that the user knows what unit he is converting
-T = ctk.CTkLabel(window, text="Temperature", font=("Arial", 24), text_color="white", bg_color="black")
+T = ctk.CTkLabel(window, text="Temperature", font=("Arial", 26), text_color="white", bg_color="black")
 T.pack(fill="both")
 
 #this is where the user can see the result of the conversion
 output_box = ctk.CTkEntry(window, width=130, font=("Arial", 14))
 output_box.configure(state="disabled")
-output_box.place(x=175, y=150)
+output_box.place(x=360, y=150)
 
 #this is where the user is supposed to write his unit he wants converted
 input_box = ctk.CTkEntry(window, width=130, font=("Arial", 14))
 # set the input type to a number
 input_box.configure(validate="key")
 input_box.configure(validatecommand=(input_box.register(lambda val: val.isdigit() or val == "." or val == "-"), '%S'))
-input_box.place(x=360, y=150)
+input_box.place(x=175, y=150)
 
 #this button frame is where all the buttons are which are used for manoeuvring through the app
 sidebar = tk.Frame(window, background="black")
@@ -34,8 +36,10 @@ sidebar.columnconfigure(0, weight=1)
 T_button = ctk.CTkButton(sidebar, text="Temperature", font=("Arial", 16))
 T_button.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-T_select = customtkinter.CTkOptionMenu(master=window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"])
-T_select.place(x=175, y=105)
+T_select_input = customtkinter.CTkOptionMenu(master=window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"], width=130)
+T_select_input.place(x=175, y=105)
+T_select_output = customtkinter.CTkOptionMenu(master=window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"], width=130)
+T_select_output.place(x=360, y=105)
 
 L_button = ctk.CTkButton(sidebar, text="Length", font=("Arial", 16))
 L_button.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
