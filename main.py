4ad2db74bffd4b5080250bc055982fd115318972
Andrei <m.andrei.antonescu@gmail.com>
2023-04-15 17:08:19 +0200

    added convert button, app can now convert temperatures

--- main.py
+++ main.py
@@ -7,22 +7,50 @@ window.title("Unit Converter")
 window.geometry("500x300")
 window.resizable(False, False)
 
+def T_convert():
+    input_unit = T_select_input.get()
+    output_unit = T_select_output.get()
+    value = entryInt.get()
+
+    if input_unit == "Celsius [°C]" and output_unit == "Fahrenheit [°F]":
+        result = value * 1.8 + 32
+    elif input_unit == "Fahrenheit [°F]" and output_unit == "Celsius [°C]":
+        result = (value - 32) / 1.8
+    elif input_unit == "kelvin [K]" and output_unit == "Celsius [°C]":
+        result = value - 273.15
+    elif input_unit == "Celsius [°C]" and output_unit == "kelvin [K]":
+        result = value + 273.15
+    elif input_unit == "Fahrenheit [°F]" and output_unit == "kelvin [K]":
+        result = (value + 459.67) * 5/9
+    elif input_unit == "kelvin [K]" and output_unit == "Fahrenheit [°F]":
+        result = value * 9/5 - 459.67
+    else:
+        result = value
+    
+    output_box_text.set(result)
+        
+
 ctk.set_appearance_mode("dark")
 #this is so that the user knows what unit he is converting
 T = ctk.CTkLabel(window, text="Temperature", font=("Arial", 26), text_color="white", bg_color="black")
 T.pack(fill="both")
 
 #this is where the user can see the result of the conversion
-output_box = ctk.CTkEntry(window, width=130, font=("Arial", 14))
+output_box_text = ctk.StringVar()
+output_box = ctk.CTkEntry(window, width=130, font=("Arial", 14), textvariable=output_box_text)
 output_box.configure(state="disabled")
 output_box.place(x=360, y=150)
 
 #this is where the user is supposed to write his unit he wants converted
-input_box = ctk.CTkEntry(window, width=130, font=("Arial", 14))
+entryInt = tk.IntVar()
+input_box = ctk.CTkEntry(window, width=130, font=("Arial", 14),  textvariable = entryInt)
 # set the input type to a number
 input_box.configure(validate="key")
 input_box.configure(validatecommand=(input_box.register(lambda val: val.isdigit() or val == "." or val == "-"), '%S'))
 input_box.place(x=175, y=150)
+
+T_convert_button = ctk.CTkButton(window, text="Convert", font=("Arial", 16), command=T_convert)
+T_convert_button.place(x=262, y=195)
 
 #this button frame is where all the buttons are which are used for manoeuvring through the app
 sidebar = tk.Frame(window, background="black")
@@ -36,9 +64,9 @@ sidebar.columnconfigure(0, weight=1)
 T_button = ctk.CTkButton(sidebar, text="Temperature", font=("Arial", 16))
 T_button.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
-T_select_input = customtkinter.CTkOptionMenu(master=window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"], width=130)
+T_select_input = customtkinter.CTkOptionMenu(window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"], width=130)
 T_select_input.place(x=175, y=105)
-T_select_output = customtkinter.CTkOptionMenu(master=window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"], width=130)
+T_select_output = customtkinter.CTkOptionMenu(window, values=["Celsius [°C]", "Fahrenheit [°F]", "kelvin [K]"], width=130)
 T_select_output.place(x=360, y=105)
 
 L_button = ctk.CTkButton(sidebar, text="Length", font=("Arial", 16))
