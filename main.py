0ea0c2ad266d244a2b1d2967c1b23c3a677bca73
Andrei <m.andrei.antonescu@gmail.com>
2023-05-05 22:01:54 +0200

    Fixed major regarding temperature conversion and implemented constants

--- main.py
+++ main.py
@@ -10,23 +10,34 @@ window.geometry("540x300")
 window.resizable(False, False)
 ctk.set_appearance_mode("dark")
 
+#define constants for the buttons
+BUTTON_FG_COLOR = ("black", "#1F6AA5")
+BUTTON_HOVER_COLOR = ("darkgrey", "#144870")
+BUTTON_FONT = customtkinter.CTkFont(family = "Arial", size = 11, weight = "bold")
+BUTTON_WIDTH = 1
+
 def convert(s:str):
     value = entry_double.get()
     input_unit = select_input.get()
     output_unit = select_output.get()
-    try:
-        conversion_factor = getattr(dictionaries, s + "_conversion_factors")[input_unit][output_unit]
-        result = conversion_factor * value
-    except KeyError:
-        result = "0"
+    if s != "t":
+        try:
+            conversion_factor = getattr(dictionaries, s + "_conversion_factors")[input_unit][output_unit]
+            result = conversion_factor * value
+        except KeyError:
+            result = "0"
+    else:
+        if input_unit == output_unit:
+            result = value
+        else:
+            result = dictionaries.t_conversion_factors[input_unit][output_unit](value)
     output_box_text.set(result)
 
 def box_reset():
     output_box_text.set("")
     entry_double.set(0)
 
-#these functions are used to show the correct widgets for each conversion type
-def show_widgets(s:str, conversion_factors:dict, title:str, values:str):
+def show_widgets(s: str, conversion_factors:dict, title:str, values:str):
     box_reset()
     convert_button.configure(command = lambda: convert(s))
     select_output.configure(values = conversion_factors)
@@ -34,26 +45,9 @@ def show_widgets(s:str, conversion_factors:dict, title:str, values:str):
     select_input.configure(values = conversion_factors)
     select_input.set(value = values[0])
     Label.configure(text = title)
-    input_box.configure(validate="key")
-    input_box.configure(validatecommand = (input_box.register(lambda val: val.isdigit() or val == "." or val == "-"), '%S'))
-
-def show_length_widgets():
-    show_widgets("l", dictionaries.l_conversion_factors, "Length", dictionaries.l_values)
-
-def show_temperature_widgets():
-    show_widgets("t", dictionaries.t_conversion_factors, "Temperature", dictionaries.t_values)
-
-def show_area_widgets():
-    show_widgets("a", dictionaries.a_conversion_factors, "Area", dictionaries.a_values)
-
-def show_volume_widgets():
-    show_widgets("v", dictionaries.v_conversion_factors, "Volume", dictionaries.v_values)
-
-def show_mass_widgets():
-    show_widgets("m", dictionaries.m_conversion_factors, "Mass", dictionaries.m_values)
-
-def show_time_widgets():
-    show_widgets("ti", dictionaries.ti_conversion_factors, "Time", dictionaries.ti_values)
+    input_box.configure(validate = "key")
+    input_box.configure(validatecommand = (
+        input_box.register(lambda val: val.isdigit() or val == "." or val == "-"), '%S'))
 
 #this is the function for the button that will copy the result to the clipboard
 def copy_to_clipboard():
@@ -76,7 +70,7 @@ output_box = ctk.CTkEntry(window,
                           font = ("Arial", 14),
                           textvariable = output_box_text)
 #disabling the entry so that the user cant input anything in it
-output_box.configure(state ="disabled")
+output_box.configure(state = "disabled")
 output_box.place(x = 360, y = 150)
 
 #this is where the user is supposed to write his unit he wants converted
@@ -86,16 +80,16 @@ input_box = ctk.CTkEntry(window,
                          font = ("Arial", 14),  
                          textvariable = entry_double)
 #set the input type to number only
-input_box.configure(validate="key")
+input_box.configure(validate = "key")
 input_box.configure(validatecommand = (input_box.register(lambda val: val.isdigit() or val == "." or val == "-"), '%S'))
-input_box.place(x=175, y=150)
+input_box.place(x = 175, y = 150)
 
 # this is the sidebar where the user can choose the conversion type
-sidebar = ctk.CTkFrame(window, fg_color = ("lightgrey","black"), corner_radius=0)
+sidebar = ctk.CTkFrame(window, fg_color = ("lightgrey","black"), corner_radius = 0)
 for x in range(6):
     sidebar.rowconfigure(x, weight = 1)
     
-sidebar.pack(side="left", fill = "y")
+sidebar.pack(side = "left", fill = "y")
 
 #label telling the user what type of conversion he is doing
 Label = ctk.CTkLabel(window, 
@@ -106,34 +100,32 @@ Label = ctk.CTkLabel(window,
 Label.pack(fill = "both", ipady = 3)
 
 categories = [
-    ("Temperature", show_temperature_widgets),
-    ("Length", show_length_widgets),
-    ("Area", show_area_widgets),
-    ("Volume", show_volume_widgets),
-    ("Mass", show_mass_widgets),
-    ("Time", show_time_widgets)
+    ("Temperature", lambda: show_widgets("t", dictionaries.t_conversion_factors, "Temperature", dictionaries.t_values)),
+    ("Length", lambda: show_widgets("l", dictionaries.l_conversion_factors, "Length", dictionaries.l_values)),
+    ("Area", lambda: show_widgets("a", dictionaries.a_conversion_factors, "Area", dictionaries.a_values)),
+    ("Volume", lambda: show_widgets("v", dictionaries.v_conversion_factors, "Volume", dictionaries.v_values)),
+    ("Mass", lambda: show_widgets("m", dictionaries.m_conversion_factors, "Mass", dictionaries.m_values)),
+    ("Time", lambda: show_widgets("ti", dictionaries.ti_conversion_factors, "Time", dictionaries.ti_values))
 ]
 
 # Create and place the buttons in the sidebar using a loop
 for row, (label, command) in enumerate(categories):
     button = ctk.CTkButton(sidebar,
-                           text=label,
-                           font=customtkinter.CTkFont(family="Arial", size=16, weight="bold"),
-                           command=command,
-                           text_color="white",
-                           fg_color=("black", "#1F6AA5"),
-                           hover_color=("darkgrey", "#144870"))
-    button.grid(row=row, column=0, padx=10, pady=10, sticky="ew")
+                           text = label,
+                           font = customtkinter.CTkFont(family="Arial", size=16, weight="bold"),
+                           command = command,
+                           fg_color = BUTTON_FG_COLOR,
+                           hover_color = BUTTON_HOVER_COLOR)
+    button.grid(row = row, column = 0, padx = 10, pady = 10, sticky = "ew")
 
 #copy to clipboard button
 copy_button = customtkinter.CTkButton(window, 
                                       text = "copy", 
-                                      font = customtkinter.CTkFont(family = "Arial", size = 11, weight="bold"), 
-                                      width = 1, 
+                                      font = BUTTON_FONT, 
+                                      width = BUTTON_WIDTH, 
                                       command = copy_to_clipboard, 
-                                      text_color="white",  
-                                      fg_color = ("black", "#1F6AA5"), 
-                                      hover_color=("darkgrey", "#144870"))
+                                      fg_color = BUTTON_FG_COLOR, 
+                                      hover_color = BUTTON_HOVER_COLOR)
 copy_button.place(x = 495, y = 150)
 
 #theme button
@@ -141,11 +133,10 @@ theme = ctk.StringVar(value = ctk.get_appearance_mode())
 theme_button = ctk.CTkButton(window, 
                              textvariable = theme, 
                              command = change_theme, 
-                             width = 1, 
-                             font = customtkinter.CTkFont(family = "Arial", size = 11, weight="bold"), 
-                             text_color="white", 
-                             fg_color = ("black", "#1F6AA5"), 
-                             hover_color=("darkgrey", "#144870"))
+                             width = BUTTON_WIDTH, 
+                             font = BUTTON_FONT, 
+                             fg_color = BUTTON_FG_COLOR, 
+                             hover_color = BUTTON_HOVER_COLOR)
 theme_button.place(x = 495, y = 195)
 
 #input dropdown menu
@@ -154,7 +145,7 @@ select_input = customtkinter.CTkOptionMenu(window,
                                                     "Fahrenheit [°F]", 
                                                     "kelvin [K]"],
                                             width = 130,
-                                            text_color="white",
+                                            text_color = "white",
                                             fg_color = ("black", "#1F6AA5"),
                                             button_color = ("black","#144870"),
                                             button_hover_color = ("darkgrey", "#144870"))
@@ -166,7 +157,7 @@ select_output = customtkinter.CTkOptionMenu(window,
                                                     "Fahrenheit [°F]", 
                                                     "kelvin [K]"],
                                             width = 130,
-                                            text_color="white",
+                                            text_color = "white",
                                             fg_color = ("black", "#1F6AA5"),
                                             button_color = ("black","#144870"),
                                             button_hover_color = ("darkgrey", "#144870"))
@@ -176,15 +167,14 @@ select_output.place(x = 360, y = 105)
 convert_button_image = customtkinter.CTkImage(Image.open("convert.png"), size = (20, 20))
 
 #convert button
-convert_button = ctk.CTkButton(window, text = "",
+convert_button = ctk.CTkButton(window, 
+                                text = "",
                                 image = convert_button_image,
-                                font = customtkinter.CTkFont(family = "Arial", size = 16, weight="bold"),
                                 command = lambda: convert("t"),
-                                text_color="white",
-                                width=21,
-                                height=21,
-                                fg_color = ("black", "#1F6AA5"),
-                                hover_color=("darkgrey", "#144870"))
+                                width = 21,
+                                height = 21,
+                                fg_color = BUTTON_FG_COLOR,
+                                hover_color = BUTTON_HOVER_COLOR)
 convert_button.place(x = 315, y = 150)
 
 window.mainloop()
